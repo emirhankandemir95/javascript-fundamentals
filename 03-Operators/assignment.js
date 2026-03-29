ea73ed3fb447ad7d8b30074b3b5780b5eab291de
Emirhan Kandemir <emirhankandemir95@gmail.com>
2026-03-29 15:09:33 +0300

    feat: if statements assignment

---
+++ 03-Operators/assignment.js
@@ -0,0 +1,10 @@
+let userAge = 31;
+if (userAge < 18) {
+    console.log("You are under age you can't enter site.")
+}
+else if (userAge >= 18 && userAge <= 30) {
+    console.log("Welcome my friend!")
+}
+else {
+    console.log("Welcome UNC!")
+}
